06da48663a5ba44f8d277048f5a14e41c8d3eff9
Mirko <bicchierai.mirko.developer@gmail.com>
2025-03-29 21:55:04 +0100

    Initial commit

---
+++ LAB_1/main.py
@@ -0,0 +1,14 @@
+import wandb
+
+def main():
+    wandb.init(
+        project="DLA_LAB_1",
+        name="experiment_stillness_reward",
+        config={
+            "learning_rate": 1e-3,
+            "epochs": 4,
+        }
+    )
+
+if __name__ == "__main__":
+    main()
