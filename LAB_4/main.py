5bb5259f34899393093a7e62cf2cb9d7712990ee
Mirko <bicchierai.mirko.developer@gmail.com>
2025-05-14 14:54:11 +0200

    First update of LAB4

--- LAB_4/main.py
+++ LAB_4/main.py
@@ -1,5 +1,20 @@
 import argparse
-from utils import str2bool, config_loggers
+
+import wandb
+from tqdm import tqdm
+
+from model import CNN
+from utils import str2bool, config_loggers, get_dataloaders, train, test, get_fake_loaders
+import torch
+import torchvision
+from torchvision.datasets import FakeData
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch import nn
+from torch import optim
+import numpy as np
+from sklearn import metrics
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Hyperparameter settings")
@@ -17,6 +32,54 @@ def get_parser():
 def main():
     args = get_parser()
     config_loggers(args)
+
+    batch_size = 256
+    num_workers = 12
+
+    train_dataloader, val_dataloader, test_dataloader, num_classes, input_size = get_dataloaders("CIFAR10", batch_size, num_workers=num_workers)
+    fake_dataloader = get_fake_loaders(batch_size, num_workers=num_workers)
+
+    for data in fake_dataloader:
+        x, y = data
+        img = x[0] # * 0.5 + 0.5 unnormalize
+        img = img.permute(1, 2, 0)  # [C, H, W] -> [H, W, C]
+        plt.imshow(img)
+        plt.title(f"Label: {train_dataloader.dataset.dataset.classes[y[0].item()]}")
+        plt.show()
+        break
+
+    print(x.shape, train_dataloader.dataset.dataset.classes[y[0]])
+    print(train_dataloader.dataset.dataset.classes)
+    class_dict = {class_name: id_class for id_class, class_name in enumerate(train_dataloader.dataset.dataset.classes)}
+    print(class_dict)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = CNN().to(device)
+
+    epochs = 200
+    lr = 1e-4
+    opt = torch.optim.Adam(params=model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=epochs)
+
+    # Train loop
+    train_bar = tqdm(range(epochs), desc=f"[Training epochs]")
+    for epoch in train_bar:
+        loss = train(model, train_dataloader, opt, device, epoch, epochs)
+        accuracy, _, val_loss = test(model, val_dataloader, device)
+        scheduler.step()
+
+        wandb.log({"Train-ES1": {"Loss": loss, "epoch": epoch},
+                   "Validation-ES1": {"Loss": val_loss, "Accuracy": accuracy, "epoch": epoch}})
+
+        train_bar.set_postfix(epoch_loss=f"{loss:.4f}")
+
+    accuracy, _, _ = test(model, test_dataloader, device)
+    wandb.log({"Test-ES1": {"Accuracy": accuracy}})
+    print("Test accuracy: {}".format(accuracy))
+
+    torch.save(model.state_dict(), "Models/CNN_pretrain.pth")
+
+
 
 if __name__ == "__main__":
     main()
--- LAB_4/utils.py
+++ LAB_4/utils.py
@@ -1,7 +1,12 @@
 
-"""
-Function used to define the type of an attribute inside of the parser for the main, to parse for example '--Residual true'
-"""
+import numpy as np
+import torch
+from sklearn.metrics import accuracy_score
+from torch.utils.data import Subset
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, CIFAR100, FakeData
+from tqdm import tqdm
+import torch.nn.functional as F
 import wandb
 
 
@@ -15,17 +20,121 @@ def str2bool(v):
 
     return None
 
+def get_fake_loaders(batch_size, num_workers):
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    fake_dataset = FakeData(size=1000, image_size=(3, 32, 32), transform=transform)
+    fake_dataloader = torch.utils.data.DataLoader(fake_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return fake_dataloader
+
+def get_dataloaders(name, batch_size, num_workers):
+    ds_train, ds_test, num_classes, input_size = None, None, 0, 0
+
+    if name == "CIFAR10":
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.4914, 0.4822, 0.4465],
+                std=[0.2470, 0.2435, 0.2616]
+            )
+        ])
+        ds_train = CIFAR10(root='./data', train=True, download=True, transform=transform)
+        ds_test = CIFAR10(root='./data', train=False, download=True, transform=transform)
+        num_classes = 10
+        input_size = 32 * 32 * 3
+
+    if name == "CIFAR100":
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.5071, 0.4867, 0.4408],
+                std=[0.2675, 0.2565, 0.2761]
+            )
+        ])
+        ds_train = CIFAR100(root='./data', train=True, download=True, transform=transform)
+        ds_test = CIFAR100(root='./data', train=False, download=True, transform=transform)
+        num_classes = 100
+        input_size = 32 * 32 * 3
+
+    # Split train into train and validation.
+    val_size = 5000
+    indices = np.random.permutation(len(ds_train))
+    ds_val = Subset(ds_train, indices[:val_size])
+    ds_train = Subset(ds_train, indices[val_size:])
+
+    train_dataloader = torch.utils.data.DataLoader(ds_train, batch_size, shuffle=True, num_workers=num_workers,pin_memory=True)
+    val_dataloader = torch.utils.data.DataLoader(ds_val, batch_size, num_workers=num_workers, pin_memory=True)
+    test_dataloader = torch.utils.data.DataLoader(ds_test, batch_size, shuffle=True, num_workers=num_workers,pin_memory=True)
+
+    return train_dataloader, val_dataloader, test_dataloader, num_classes, input_size
+
+def test(model, dataloader, device):
+    model.eval()
+    predictions = []
+    gts = []
+    losses = []
+
+    top5_correct = 0
+    total_samples = 0
+
+    test_bar = tqdm(dataloader, desc=f"[Test/Validation]", leave=False)
+    with torch.no_grad():
+        for data, labels in test_bar:
+            data = data.to(device)
+            labels = labels.to(device)
+
+            logits = model(data)
+            loss = F.cross_entropy(logits, labels)
+            prob = F.softmax(logits, dim=1)
+            pred = torch.argmax(prob, dim=1)
+
+            # Top-5 accuracy calculation
+            top5 = torch.topk(prob, k=5, dim=1).indices
+            top5_correct += sum([labels[i].item() in top5[i] for i in range(labels.size(0))])
+            total_samples += labels.size(0)
+
+            gts.append(labels.cpu().numpy())
+            predictions.append(pred.cpu().numpy())
+            losses.append(loss.item())
+            test_bar.set_postfix(minibatch_loss=f"{loss.item():.4f}")
+
+    top5_accuracy = top5_correct / total_samples
+    top1_accuracy = accuracy_score(np.hstack(gts), np.hstack(predictions))
+    avg_loss = np.mean(losses)
+
+    return top1_accuracy, top5_accuracy, avg_loss
+
+
+def train(model, dataloader, opt, device, epoch, epochs):
+    model.train()
+    model = model.to(device)
+    losses = []
+    train_bar = tqdm(dataloader, desc=f"Epoch {epoch + 1}/{epochs} [Training]", leave=False)
+    for data, labels in train_bar:
+        data = data.to(device)
+        labels = labels.to(device)
+        opt.zero_grad()
+        logits = model(data)
+        loss = F.cross_entropy(logits, labels)
+        loss.backward()
+        opt.step()
+        losses.append(loss.item())
+        train_bar.set_postfix(minibatch_loss=f"{loss.item():.4f}")
+    return np.mean(losses)
 
 """
 Function to define the wandb parameters for the main.py 
 """
-
 def config_loggers(args):
 
     exp_name = "_New"
 
     wandb.init(
-        project="DLA_LAB_3",
+        project="DLA_LAB_4",
         config=vars(args),
         name=exp_name,
     )
